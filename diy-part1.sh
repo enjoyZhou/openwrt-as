fd5aa61ebcb26d375fdb7bb744dd42dc55fa3b5d
enjoyzhou <369141517@qq.com>
2020-09-07 12:11:54 +0800

    32m

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,10 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# CP conf
+cp -rf openwrt-ext/target/linux/ramips/base-files/etc/board.d/01_leds target/linux/ramips/base-files/etc/board.d/
+cp -rf openwrt-ext/target/linux/ramips/base-files/etc/board.d/02_network target/linux/ramips/base-files/etc/board.d/
+cp -rf openwrt-ext/target/linux/ramips/base-files/lib/ramips.sh target/linux/ramips/base-files/lib/
+cp -rf openwrt-ext/target/linux/ramips/dts/routerPi1_32M.dts target/linux/ramips/dts/
+cp -rf openwrt-ext/target/linux/ramips/image/mt76x8.mk arget/linux/ramips/image/
